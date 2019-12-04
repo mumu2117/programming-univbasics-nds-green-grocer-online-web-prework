911747eacf0c69522e8453d088e0c9cf7943ac9a
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-04 13:12:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -5,10 +5,18 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+ consolidated_cart = {}
+  cart.each do |item_data|
+    item_data.each do |item, details|
+      if consolidated_cart.keys.include?(item)
+        consolidated_cart[item][:count] += 1
+      else
+        consolidated_cart[item] = details
+        consolidated_cart[item][:count] = 1
+      end
+    end
+  end
+  consolidated_cart
 end
 
 def apply_coupons(cart, coupons)
